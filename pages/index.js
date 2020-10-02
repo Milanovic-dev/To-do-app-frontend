a4b3f41314be0ad42c63e5f8d614ef822dc11471
Nikola Milanovic <nikolamilanovic21@gmail.com>
2020-10-02 10:47:40 +0200

    remove unused code

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,3 @@
-import Head from "next/head";
-import { actionChannel } from "redux-saga/effects";
-import styles from "../styles/Home.module.css";
-import { setLoading } from "../store/actions/loaderActions";
-import { useDispatch } from "react-redux";
-
-import Header from "../components/header";
-
 export default function Home() {
-    const dispatch = useDispatch();
-
     return <div>Index</div>;
 }
