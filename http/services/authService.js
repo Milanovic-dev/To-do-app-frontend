3013121101a7a88ff248d38358be450cf9aa20b1
Nikola Milanovic <nikolamilanovic21@gmail.com>
2020-10-05 14:47:30 +0200

    add registration

--- http/services/authService.js
+++ http/services/authService.js
@@ -2,22 +2,26 @@ import HttpService from "./httpService";
 
 const ENDPOINTS = {
     LOGIN: "/auth/login",
+    REGISTER: "/auth/register",
 };
 
 class AuthService extends HttpService {
     constructor() {
         super();
         this.login = this.login.bind(this);
+        this.register = this.register.bind(this);
     }
 
     async login(payload) {
-        const { accessToken } = await this.client.post(
+        const { access_token } = await this.client.post(
             ENDPOINTS.LOGIN,
             payload
         );
 
-        this.setAuthroization(accessToken);
+        this.setAuthroization(access_token);
     }
+
+    register = (payload) => this.client.post(ENDPOINTS.REGISTER, payload);
 
     setAuthroization(accessToken) {
         localStorage.setItem("token", accessToken);
--- pages/register.js
+++ pages/register.js
@@ -1,18 +1,21 @@
 import { Formik, Field, Form } from "formik";
 import TextField from "../components/TextField";
 import Button from "../components/Button";
-import { loginSchema } from "../components/forms/validation/auth";
 import LinkHref from "../components/LinkHref";
-import { useDispatch, useSelector } from "react-redux";
-import { login, setAuthError } from "../store/actions/authActions";
-import { setLoading } from "../store/actions/loaderActions";
 import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { registerSchema } from "../components/forms/validation/auth";
+import { setLoading } from "../store/actions/loaderActions";
+import { register, setAuthError } from "../store/actions/authActions";
 import { authErrorSelector } from "../store/selectors/authSelector";
 import { loadingSelector } from "../store/selectors/loaderSelector";
 
-const Login = () => {
+const Register = () => {
     const dispatch = useDispatch();
-    const handleLogin = useCallback((values) => dispatch(login(values)), []);
+    const handleRegister = useCallback(
+        (values) => dispatch(register(values)),
+        []
+    );
 
     const error = useSelector(authErrorSelector);
     const loading = useSelector(loadingSelector);
@@ -27,18 +30,29 @@ const Login = () => {
     return (
         <div className="loginForm">
             <div style={{ fontSize: 20, marginBottom: 30, fontWeight: "bold" }}>
-                Please login
+                Please sign up
             </div>
             <Formik
-                initialValues={{ email: "", password: "" }}
-                onSubmit={handleLogin}
-                validationSchema={loginSchema}
+                initialValues={{
+                    name: "",
+                    email: "",
+                    password: "",
+                    confirmPassword: "",
+                }}
+                onSubmit={handleRegister}
+                validationSchema={registerSchema}
             >
                 {() => (
                     <Form>
                         <Field
                             as={TextField}
-                            label="Email"
+                            label="Name"
+                            name="name"
+                            type="text"
+                        ></Field>
+                        <Field
+                            as={TextField}
+                            label="Email address"
                             name="email"
                             type="email"
                         ></Field>
@@ -48,20 +62,29 @@ const Login = () => {
                             name="password"
                             type="password"
                         ></Field>
-                        <Button type="submit" loading={loading}>
-                            Log in
+                        <Field
+                            as={TextField}
+                            label="Confirm Pasword"
+                            name="confirmPassword"
+                            type="password"
+                        ></Field>
+                        <Button loading={loading} type="submit">
+                            Sign up
                         </Button>
                         <p className="error">{error}</p>
                     </Form>
                 )}
             </Formik>
+            <p style={{ fontSize: 11 }}>
+                By continuing you agree to Todoapp's Terms of Service and
+                Privacy Policy.
+            </p>
             <div className="separator"></div>
             <p>
-                Don't have an account?{" "}
-                <LinkHref value="Register" href="/register" />
+                Already registered? <LinkHref value="Login" href="/login" />
             </p>
         </div>
     );
 };
 
-export default Login;
+export default Register;
